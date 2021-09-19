d08193d3e868a46779686d5b9ccf6d5b90274eaf
Brian Shimkus <brianshimkus1@gmail.com>
2021-09-19 16:20:57 -0500

    Added in Montserrat font

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,10 @@ export default function Layout({ title, keywords, description, children }) {
 				<title>{title}</title>
 				<meta name='description' content={description} />
 				<meta name='keywords' content={keywords} />
+				<link
+					href='https://fonts.googleapis.com/css2?family=Montserrat'
+					rel='stylesheet'
+				/>
 			</Head>
 			<div>{children}</div>
 		</div>
