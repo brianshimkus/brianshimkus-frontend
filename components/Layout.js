7e1fd6730159a2f22b0a4b6cbe16acf7eddd4304
Brian Shimkus <brianshimkus1@gmail.com>
2021-09-19 14:18:14 -0500

    Built out Layout component

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+export default function Layout({ title, keywords, description, children }) {
+	const router = useRouter()
+	return (
+		<div>
+			<Head>
+				<title>{title}</title>
+				<meta name='description' content={description} />
+				<meta name='keywords' content={keywords} />
+			</Head>
+			<div>{children}</div>
+		</div>
+	)
+}
+
+Layout.defaultProps = {
+	title: 'Brian Shimkus | Personal Site',
+	description: 'The personal site of Brian Shimkus',
+	keywords: 'entrepreneur, musician, software engineer',
+}
