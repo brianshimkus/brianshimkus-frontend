c586e94bfa8d505ea68f29bc5aeda4e35f0a1a47
Brian Shimkus <brianshimkus1@gmail.com>
2021-11-15 23:47:08 -0600

    Initial sub pages creation resolves #7

---
+++ pages/music.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function MusicPage() {
+	return (
+		<Layout>
+			<h1>Music Page</h1>
+		</Layout>
+	)
+}
---
+++ pages/reef.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function ReefPage() {
+	return (
+		<Layout>
+			<h1>Reef Page</h1>
+		</Layout>
+	)
+}
