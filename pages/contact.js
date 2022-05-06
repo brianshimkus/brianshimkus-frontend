0f35dcee457b6d7dde57a85581b5bd9191662628
Brian Shimkus <brianshimkus1@gmail.com>
2022-05-05 23:58:59 -0500

    Scaffolded additional pages

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function ContactPage() {
+	return (
+		<Layout>
+			<h1>Contact Page</h1>
+		</Layout>
+	)
+}
---
+++ pages/entrepreneurship.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function EntrepreneurshipPage() {
+	return (
+		<Layout>
+			<h1>Entrepreneurship Page</h1>
+		</Layout>
+	)
+}
